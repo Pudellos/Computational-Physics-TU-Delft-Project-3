48d581012bd44cb5cd3db63f2cc2529c9b3fbd99
Klara Chmiel <k.m.chmiel@student.tudelft.nl>
2021-05-26 14:03:37 +0200

    solbing lindbad with imposed B-filed added

--- main.py
+++ main.py
@@ -26,10 +26,27 @@ sigma_p = np.matrix([[0, 0],
 sigma_m = np.matrix([[0, 1],
                      [0, 0]])
 
+
+### Magnetic Field present: ########################################################################
+## Angular mmtm operators ## # -> hbar = 1
+S_x = 1/2*np.matrix([[0, 1],
+                [1, 0]]) 
+S_y = 1/2*np.matrix([[0, -1j],
+                [1j, 0]])
+S_z = 1/2*np.matrix([[1, 0],
+                [0, -1]])
+gamma = 1 # gyromagnetic ratio - needs proper value
+
+B = [1 ,1, 1]
+
+H_B = -gamma*(B[0]*S_x + B[1]*S_y + B[2]*S_z)
+
+####################################################################################################
+
+
 ## Hamiltonian (spin-1/2 system, in B-field)##
 omega_0 = 1
 H = -(1/2)* omega_0 * sigma_z # -> hbar = 1
-
 
 timesteps = 1000
 dt = 0.01
@@ -41,6 +58,7 @@ k = [k_p, k_m, k_z]
 rho = np.ndarray(shape = (timesteps, 2, 2), dtype = np.complex)
 rho[0] = rho_0
 
+
 P = np.zeros(timesteps)
 P[0] = np.trace(np.matmul(outer_product(up,up), rho[0]))
 for t in range(1, timesteps):
@@ -50,7 +68,7 @@ for t in range(1, timesteps):
     for i in range(len(L)):
         A += k[i] * (np.matmul(np.matmul(L[i], rho[t-1]), L[i].getH()) - (1/2) * anti_commutator(np.matmul(L[i].getH(), L[i]), rho[t-1]))
     
-    rho[t] = rho[t-1] + (-1j * commutator(H, rho[t-1]) + A) * dt
+    rho[t] = rho[t-1] + (-1j * commutator(H, rho[t-1]) + A) * dt ########### CHANGE H TO H_B TO GET STUFF IN B-FIELD ##############!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     print(rho[t-1])  
     print(round(np.trace(rho[t-1]),2))
