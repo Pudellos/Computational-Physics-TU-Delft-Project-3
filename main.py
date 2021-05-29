01aa54e4b54e29c09ade8adb7ab9af2746407d37
Klara Chmiel <k.m.chmiel@student.tudelft.nl>
2021-05-29 18:27:47 +0200

    trying to fit decay times equation

--- main.py
+++ main.py
@@ -71,14 +71,46 @@ for t in range(1, timesteps):
     
     rho[t] = rho[t-1] + (-1j * commutator(H, rho[t-1]) + A) * dt ########### CHANGE H TO H_B TO GET STUFF IN B-FIELD ##############!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    print(rho[t-1])  
-    print(round(np.trace(rho[t-1]),2))
+#     print(rho[t-1])  
+#     print(round(np.trace(rho[t-1]),2))
     P[t] = np.trace(np.matmul(outer_product(up,up), rho[t-1]))
     Fidel[t]=(np.trace(np.power(np.power(rho[t], 1/2)*rho_0*np.power(rho[t], 1/2), 1/2)))**2 #Calculation of the fidelity with respect to rho_0
 
 
 plt.plot(np.arange(0,timesteps), P)
 plt.show()
+
+
+##### FITTING THE DECAY TIMES EQUATIONS FROM LECTURE NOTES 'LECTURE 8' ################################# 
+
+rho11=np.zeros(len(rho))
+for i in range(len(rho)):
+    rho11[i]=rho[i][0][0]
+
+plt.plot(np.arange(0,timesteps), rho11,'ro',label='data',markersize=2)
+plt.xlabel('timesteps')
+plt.ylabel('rho_11')
+
+from scipy.odr import *
+def function(p, x):
+    gamma, c = p
+    return (c*(1-np.exp(-gamma*x)))
+quad_model = Model(function)
+x = np.arange(0,timesteps)
+y = np.array(rho11)
+x_err = np.full(len(x),np.finfo(np.float32).eps)
+y_err = np.full(len(x),np.finfo(np.float32).eps)
+data = RealData(x, y, sx=x_err, sy=y_err)
+odr = ODR(data, quad_model, beta0=[0., 1.])
+out = odr.run()
+x_fit = np.linspace(x[0], x[-1], 1000)
+y_fit = function(out.beta, x_fit)
+plt.plot(x_fit, y_fit,color='blue',linewidth=2, label='data ODR fit')
+plt.legend()
+plt.show()
+print('gamma=',out.beta[0])
+######################################################################################3
+
 
 plt.plot(np.arange(0,timesteps), Fidel)
 plt.show()
